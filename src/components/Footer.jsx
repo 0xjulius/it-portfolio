19feeb76eeb494d5a55fea0eb64a69258fa780ee
JULIUS AALTO <72438387+0xjulius@users.noreply.github.com>
2024-03-23 00:40:40 +0200

    10.2
    removed video on footer, and added normal bg

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,13 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import footerbg from "../images/footerbg.mp4";
+import footerbg from "../images/aaabstract.png"
 
 const Footer = () => {
   return (
     <footer className="relative text-white py-16 mt-12">
+      <img src={footerbg} alt="Footer Background" className="absolute inset-0 w-full h-full object-fit blur-lg " />
       <div className="container mx-auto flex flex-col md:flex-row items-center justify-between px-4">
-        <video
-          autoPlay
-          loop
-          muted
-          className="absolute inset-0 w-full h-full object-cover brightness-150 blur-xl"
-        >
-          <source src={footerbg} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
         <div className="flex items-center mb-4 md:mb-0 z-10">
           <span className="text-xl font-semibold"></span>
           <div className="ml-4 flex space-x-4 text-3xl">
