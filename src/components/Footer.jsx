dc00d45d7e83978dcc73266adfa2fd74b14e6621
JULIUS AALTO <72438387+0xjulius@users.noreply.github.com>
2024-03-23 00:04:09 +0200

    update 10.0
    added amazing video footer
    
    Thanks to StefWithAnF https://www.pexels.com/video/blue-droplets-of-dye-on-a-liquid-producing-artistic-design-3866539/

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import footerbg from "../images/footerbg.mp4";
 
 const Footer = () => {
   return (
-    <footer className="bg-[#1d2947] text-white py-8 mt-10">
+    <footer className="relative text-white py-16 mt-12">
       <div className="container mx-auto flex flex-col md:flex-row items-center justify-between px-4">
-        <div className="flex items-center mb-4 md:mb-0">
-          <span className="text-lg font-semibold"></span>
+      <video autoPlay loop muted className="absolute inset-0 w-full h-full object-cover brightness-150 blur-lg">
+        <source src={footerbg} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+        <div className="flex items-center mb-4 md:mb-0 z-10">
+          <span className="text-xl font-semibold"></span>
           <div className="ml-4 flex space-x-4 text-3xl">
             <a
               href="https://github.com/0xjulius"
@@ -27,7 +32,7 @@ const Footer = () => {
             </a>
           </div>
         </div>
-        <div className="text-center md:text-right">
+        <div className="text-center md:text-right z-10 text-xl font-semibold">
           <p className="mb-2">
             Contact me: <a className="underline hover:no-underline cursor-pointer" href="mailto:julius@juliusaalto.com">julius@juliusaalto.com</a> <br/>All emails are promptly directed to the appropriate recipient.
           </p>
