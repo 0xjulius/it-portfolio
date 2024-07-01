d413c3c91c12e1a44649d528d7df86509d450da3
JULIUS AALTO <72438387+0xjulius@users.noreply.github.com>
2024-07-01 03:54:28 +0300

    update 14.1
    added new photogallery website showcasing my photography

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -19,7 +19,7 @@ const Gallery = () => {
             <p className="mx-auto text-xl ctext font-semibold card px-10 p-10">
             <h1 className="ptx font-semibold mb-6 text-2xl"><FontAwesomeIcon icon={faCamera} className="mr-4" />Photography Skills</h1>
             My extensive experience of over 10 years in <span className="ptx2 font-semibold">photography</span> adds a unique perspective to my skills. I'm adept at capturing moments and transforming them into enduring memories. This ability not only showcases my <span className="ptx2 font-semibold">creativity</span> but also demonstrates my <span className="ptx2 font-semibold">attention to detail</span> and commitment to excellence.<br/> <br/>
-            <a href="https://juliusaalto.com/photography/" rel="noreferrer" className="underline hover:no-underline ptx2 font-bold text-2xl" target="_blank">View my gallery</a>
+            <a href="https://visionbyjulius.vercel.app/" rel="noreferrer" className="underline hover:no-underline ptx2 font-bold text-2xl" target="_blank">Check out my new gallery!</a>
             </p>
 
           </div>
