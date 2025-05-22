03f06ead116f05e3013393eb46c36ac9e19d88d9
JULIUS AALTO <julius.aalto@gmail.com>
2025-05-22 04:31:54 +0300

    update 15.4
    corrected link on gallery.jsx

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -12,7 +12,7 @@ const Gallery = () => {
             <p className="mx-auto text-xl ctext font-semibold card px-10 p-10">
             <h1 className="ptx font-semibold mb-6 text-2xl"><FontAwesomeIcon icon={faPalette} className="mr-4" />Visual Design</h1>
             Creating impactful  <span className="ptx2 font-semibold">visual experiences</span> is one of my passions. I've always been drawn to <span className="ptx2 font-semibold">design, branding, and illustration</span> - there's something about bringing ideas to life through <span className="ptx2 font-semibold">colors, shapes, and typography</span> that just clicks with me. I'm passionate about exploring the endless possibilities of visual storytelling. <br/> <br/>
-            <a href="https://juliusaalto.com/illustrations/" rel="noreferrer" className="underline hover:no-underline ptx2 font-bold text-2xl" target="_blank">View my work</a>
+            <a href="https://behance.net/juliusaalto" rel="noreferrer" className="underline hover:no-underline ptx2 font-bold text-2xl" target="_blank">View my work</a>
             </p>
           </div>
           <div className="w-full lg:w-[calc(50%-20px)] flex items-stretch">
